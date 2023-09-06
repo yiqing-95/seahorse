9ea337cb78f74d6585a890743b807aabbdb550bd
yiqing-95 <61391362@qq.com>
2023-09-06 23:54:04 +0800

    support app extension functionality

--- examples/single_app.rs
+++ examples/single_app.rs
@@ -10,6 +10,7 @@ fn main() {
         .description(env!("CARGO_PKG_DESCRIPTION"))
         .usage("single_app [args]")
         .version(env!("CARGO_PKG_VERSION"))
+        .app_data(String::from("some app_name"))
         .action(action)
         .flag(
             Flag::new("bye", FlagType::Bool)
@@ -21,6 +22,17 @@ fn main() {
 }
 
 fn action(c: &Context) {
+  
+    // NOTE: this strange access pattern 
+    if let Some(app_data) =  c.extensions_mut().get::<String>(){
+
+        println!("app-data: {:#?}", app_data) ;
+    }
+
+    let d = std::cell::Ref::map(c.extensions(), |ext| ext.get::<String>().unwrap());
+    println!("app-data2: {:#?}", *d) ;
+
+
     if c.bool_flag("bye") {
         println!("Bye, {:?}", c.args);
     } else {
